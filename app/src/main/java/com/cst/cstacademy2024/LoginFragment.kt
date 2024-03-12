7adfc42efbf1f7d3079e9081cc181c5e90ca6ef7
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-03-12 17:23:41 +0200

    Lab 3 (12 mar)

--- app/src/main/java/com/cst/cstacademy2024/LoginFragment.kt
+++ app/src/main/java/com/cst/cstacademy2024/LoginFragment.kt
@@ -1,12 +1,20 @@
 package com.cst.cstacademy2024
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.cst.cstacademy2024.helpers.extensions.logErrorMessage
+import com.cst.cstacademy2024.models.LoginModel
 
 class LoginFragment : Fragment() {
 
@@ -23,11 +31,37 @@ class LoginFragment : Fragment() {
 
         val registerButton = view.findViewById<Button>(R.id.btn_register)
         registerButton.setOnClickListener(::goToRegister)
+
+        val loginButton = view.findViewById<Button>(R.id.btn_id)
+        loginButton.setOnClickListener { doLogin() }
     }
 
     private fun goToRegister(view: View) {
         val action = LoginFragmentDirections.actionFragmentLoginToFragmentRegister()
         findNavController().navigate(action)
     }
+
+    private fun doLogin() {
+        val username = view?.findViewById<EditText>(R.id.et_user_name)?.text?.toString() ?: ""
+        val password = view?.findViewById<EditText>(R.id.et_password)?.text?.toString() ?: ""
+        val loginModel = LoginModel(username, password)
+        // Call the login method from the view model
+
+        val queue = Volley.newRequestQueue(context ?: return)
+        val url = "https://fakestoreapi.com/products"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            Response.Listener<String> { response ->
+                "Success".logErrorMessage()
+            },
+            Response.ErrorListener {
+                "Error".logErrorMessage()
+            })
+
+        queue.add(stringRequest)
+    }
+
 
 }
---
+++ app/src/main/java/com/cst/cstacademy2024/models/LoginModel.kt
@@ -0,0 +1,5 @@
+package com.cst.cstacademy2024.models
+
+class LoginModel(
+    val username: String,
+    val password: String)
